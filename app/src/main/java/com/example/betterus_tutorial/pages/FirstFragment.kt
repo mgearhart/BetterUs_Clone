6604386b5139b50b4e153420514b6ae9616fdd0c
mgearhart <125161289+mgearhart@users.noreply.github.com>
2024-03-17 00:52:33 -0700

    Update FirstFragment.kt

--- app/src/main/java/com/example/betterus_tutorial/pages/FirstFragment.kt
+++ app/src/main/java/com/example/betterus_tutorial/pages/FirstFragment.kt
@@ -28,6 +28,8 @@ class FirstFragment : Fragment() {
     val firebaseUser = firebaseAuth.currentUser
     val userRef: DatabaseReference = fireDB.getReference("users").child(firebaseUser!!.uid)
 
+    private lateinit var goalInfo: GoalInfo
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,113 +42,67 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+            // Fetch GoalInfo from Firebase
+            userRef.child("goalInfo").addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    // Deserialize GoalInfo from the dataSnapshot
+                    val goalInfoFromFirebase: GoalInfo? = dataSnapshot.getValue(GoalInfo::class.java)
+
+                    // Handle the retrieved GoalInfo
+                    goalInfoFromFirebase?.let {
+                        goalInfo = it // Store GoalInfo here
 
 
 
-        userRef.child("goalInfo").addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Deserialize GoalInfo from the dataSnapshot
-                val goalInfo: GoalInfo? = dataSnapshot.getValue(GoalInfo::class.java)
+                    // Set the initial progress
+                    sharedViewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+                    val caloriesNeeded = goalInfo?.caloriesGained ?: 0
+                    val numMed = goalInfo?.numMeditation ?: 0
+                    val numEx = goalInfo?.numExercise ?: 0
+                    val caloriesToBeBurned = goalInfo?.caloriesBurnt ?: 0
 
-                // Handle the retrieved GoalInfo
-                goalInfo?.let {
+                    // Set the initial progress
+                    binding.progressBar1.progress = calculateProgress(sharedViewModel.caloriesBurned, caloriesToBeBurned)
+                    binding.progressBar4.progress = calculateProgress(sharedViewModel.caloriesEaten, caloriesNeeded)
+                    binding.progressBar5.progress = calculateProgress(sharedViewModel.meditationDone, numMed)
 
-                    // Set the goal information in the ViewModel
-                    sharedViewModel.setGoalInfo(it)
-                }
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Handle cancellation, if needed
-            }
-        })
+                    updateProgressText(binding.progressBar1.progress, binding.textView1)
+                    updateProgressText(binding.progressBar4.progress, binding.textView3)
+                    updateProgressText(binding.progressBar5.progress, binding.textView4)
 
 
+                                    //This is the overall progress bar
+                    val sum = binding.progressBar1.progress + binding.progressBar4.progress + binding.progressBar5.progress
+                    binding.progressBar6.progress = if(sum == 0){
+                        0
+                    } else if (binding.progressBar1.progress >= 100 &&
+                        binding.progressBar4.progress >= 100 &&
+                        binding.progressBar5.progress >= 100
+                    ) {
+                        100
+                    } else {
+                        (sum / 3)
+                    }
+                    updateProgressText(binding.progressBar6.progress, binding.textView8)
+                    }
+
+
+                    }
+
+                            override fun onCancelled(databaseError: DatabaseError) {
+                                // Handle cancellation, if needed
+                            }
+                        })
+    }
 
 
 
-
-
-
-
-
-
-
-        // Set the initial progress
-        sharedViewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
-
-        binding.progressBar1.progress = sharedViewModel.progress1.value ?: 0
-        binding.progressBar4.progress = sharedViewModel.progress2.value ?: 0
-        binding.progressBar5.progress = sharedViewModel.progress3.value ?: 0
-
-        // Set the initial progress
-        sharedViewModel.progress1.observe(viewLifecycleOwner) { progress ->
-            val caloriesBurnt = sharedViewModel.caloriesBurned
-            val caloriesToBeBurned = sharedViewModel.caloriesToBeBurned
-
-            // Calculate progress as a float to preserve decimal values
-            val calculatedProgress = if (caloriesToBeBurned != null && caloriesToBeBurned > 0) {
-                ((caloriesBurnt.toFloat() / caloriesToBeBurned.toFloat()) * 100).coerceAtMost(100f)
-            } else {
-                0f // Default to 0 if caloriesToBeBurned is null or 0
-            }.toInt() // Convert the calculated progress to an integer
-
-            binding.progressBar1.progress = calculatedProgress
-            updateProgressText(calculatedProgress, binding.textView1)
+    private fun calculateProgress(currentValue: Int, targetValue: Int): Int {
+        return if (targetValue > 0) {
+            ((currentValue.toFloat() / targetValue.toFloat()) * 100).coerceAtMost(100f).toInt()
+        } else {
+            0
         }
-
-        sharedViewModel.progress2.observe(viewLifecycleOwner) { progress ->
-            val caloriesEaten = sharedViewModel.caloriesEaten
-            val caloriesNeeded = sharedViewModel.caloriesNeeded
-
-            // Calculate progress as a float to preserve decimal values
-            val calculatedProgress = if (caloriesNeeded > 0) {
-                ((caloriesEaten.toFloat() / caloriesNeeded.toFloat()) * 100).coerceAtMost(100f)
-            } else {
-                0f // Default to 0 if caloriesNeeded is 0
-            }.toInt() // Convert the calculated progress to an integer
-
-            binding.progressBar4.progress = calculatedProgress
-            updateProgressText(calculatedProgress, binding.textView3)
-        }
-
-        sharedViewModel.progress3.observe(viewLifecycleOwner) { progress ->
-            val numMeditation = sharedViewModel.numMed ?: 0 // Get the value of numEx from SharedViewModel, default to 0 if null
-
-            // Calculate progress as a float to preserve decimal values
-            val calculatedProgress = if (numMeditation > 0) {
-                ((progress.toFloat() / numMeditation.toFloat()) * 100).coerceAtMost(100f)
-            } else {
-                0f // Default to 0 if numMeditation is 0
-            }.toInt() // Convert the calculated progress to an integer
-
-            binding.progressBar5.progress = calculatedProgress
-            updateProgressText(calculatedProgress, binding.textView4)
-        }
-
-//This is the overall progress bar
-        sharedViewModel.progress3.observe(viewLifecycleOwner) { progress ->
-            val sum = binding.progressBar1.progress + binding.progressBar4.progress + binding.progressBar5.progress
-
-            if(sum == 0){
-                binding.progressBar6.progress = 0
-            } else if (binding.progressBar1.progress >= 100 &&
-                binding.progressBar4.progress >= 100 &&
-                binding.progressBar5.progress >= 100
-            ) {
-
-                binding.progressBar6.progress = 100
-                updateProgressText(100, binding.textView8)
-            } else {
-                binding.progressBar6.progress = (sum/3)
-                updateProgressText((sum/3), binding.textView8)
-            }
-
-        }
-
-
-
-
 
     }
 
@@ -163,63 +119,46 @@ class FirstFragment : Fragment() {
 }
 
 class SharedViewModel : ViewModel() {
-    // Progress counter variables
-    private var goalInfo: GoalInfo? = null
-    val progress1 = MutableLiveData<Int>().apply { value = 0 }
-    val progress2 = MutableLiveData<Int>().apply { value = 0 }
-    val progress3 = MutableLiveData<Int>().apply { value = 0 }
+
+    private var goalInfo2: GoalInfo? = null
 
     val fireDB = FirebaseDatabase.getInstance()
     val firebaseAuth = FirebaseAuth.getInstance()
     val firebaseUser = firebaseAuth.currentUser
     val userRef: DatabaseReference = fireDB.getReference("users").child(firebaseUser!!.uid)
 
-
-    val caloriesNeeded: Int
-        get() = goalInfo?.caloriesGained ?: 0
-
-    val numMed: Int?
-        get() = goalInfo?.numMeditation
-
-    val numEx: Int?
-        get() = goalInfo?.numExercise
-
-    val caloriesToBeBurned: Int?
-        get() = goalInfo?.caloriesBurnt
-
-    // Locally modified variables
     var caloriesEaten: Int = 0
     var caloriesBurned: Int = 0
-
-
-    // Function to increment progress counters
-    fun incrementProgress(progress: MutableLiveData<Int>) {
-        progress.value = (progress.value ?: 0) + 1
-    }
-
-    // Function to set GoalInfo
-    fun setGoalInfo(info: GoalInfo) {
-        goalInfo = info
-    }
-
-    // Function to get GoalInfo
-    fun getGoalInfo(): GoalInfo? {
-        return goalInfo
-    }
+    var exercisesDone: Int = 0
+    var meditationDone: Int = 0
 
     fun updateCaloriesEaten(calories: Int) {
         caloriesEaten += calories
         saveUserGoal()
     }
-
     fun updateCaloriesBurnt(calories: Int) {
         caloriesBurned += calories
         saveUserGoal()
     }
+    fun updateMedDone(num: Int) {
+        meditationDone += num
+        saveUserGoal()
+
+    }fun updateExDone(num: Int) {
+        exercisesDone += num
+        saveUserGoal()
+    }
+
 
     fun saveUserGoal(){
-        // Override user's old current goal status upon exiting app
+
+        goalInfo2?.numExercise = exercisesDone
+        goalInfo2?.numMeditation = meditationDone
+        goalInfo2?.caloriesBurnt = caloriesBurned
+        goalInfo2?.caloriesGained = caloriesEaten
+
+
         userRef.child("currentGoalStatus")
-            .child("currentStatus").setValue(goalInfo);
+            .child("currentStatus").setValue(goalInfo2);
     }
 }
