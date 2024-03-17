2d75716d9f4a9390349c831fa0cc2155368c44e1
mgearhart <125161289+mgearhart@users.noreply.github.com>
2024-03-17 14:35:22 -0700

    Added chaquopy

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,5 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.chaquo.python") version "15.0.1" apply false
 }
